a004bb586d9c5f1097dfd3423f8fecfc38baaea0
Naohiro2g <587586+Naohiro2g@users.noreply.github.com>
2024-11-21 16:08:12 +0900

    test with sea_lantern

--- src/main/java/com/guithub/TeThoLaPot/reore/event/BreakEvent.java
+++ src/main/java/com/guithub/TeThoLaPot/reore/event/BreakEvent.java
@@ -10,23 +10,27 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.server.level.ServerLevel;
 
 @Mod.EventBusSubscriber(modid = RE_Ore.MOD_ID)
 public class BreakEvent {
     @SubscribeEvent
     public static void blockBreak(BlockEvent.BreakEvent event){
 
-        LevelAccessor level = event.getLevel();
+        LevelAccessor levelAccessor = event.getLevel();
+        ServerLevel level = (ServerLevel) levelAccessor;
         Player player = event.getPlayer();
         BlockPos pos = event.getPos();
-
-        BlockState state = level.getBlockState(pos);
+        BlockPos pos1 = pos.north();
+        BlockPos pos2 = pos.south();
 
         if (!player.isCreative()){
-          if (state.is(RegenTags.Blocks.CAN_REGEN)){
-                System.out.println("BREAKE");
-                level.setBlock(pos, ModBlocks.TEST_ORE.get().defaultBlockState(),1);
-          }
+            // if (state.is(RegenTags.Blocks.CAN_REGEN)){
+            System.out.println("BREAK!");
+            level.setBlock(pos, ModBlocks.TEST_ORE.get().defaultBlockState(), 2);
+            level.setBlock(pos1, Blocks.SEA_LANTERN.defaultBlockState(), 2);
+            level.setBlock(pos2, Blocks.SEA_LANTERN.defaultBlockState(), 2);
         }
     }
 }
